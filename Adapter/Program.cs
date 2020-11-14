fdfb590942e3389f15b7cf4baa8a8b5ac323f23b
Squikle <55331475+Squikle@users.noreply.github.com>
2020-11-14 17:38:54 +0200

    Adapter Added

---
+++ Adapter/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Human human = new Human();
+            ITypingData keyboard = new Keyboard();
+            human.InputData(keyboard);
+
+            Console.WriteLine();
+
+            IWriting pen = new Pen();
+            ITypingData typingDataPen = new WritingToTypingData(pen);
+            human.InputData(typingDataPen);
+
+            Console.ReadKey();
+        }
+
+        class Human
+        {
+            public void InputData(ITypingData typingData)
+            {
+                typingData.Type();
+            }
+        }
+
+        interface ITypingData
+        {
+            void Type();
+        }
+
+        class Keyboard : ITypingData
+        {
+            public void Type()
+            {
+                Console.WriteLine("Клавиатура печатает");
+            }
+        }
+
+        interface IWriting
+        {
+            void Write();
+        }
+
+        class Pen : IWriting
+        {
+            public void Write()
+            {
+                Console.WriteLine("Ручка пишет");
+            }
+        }
+
+        class WritingToTypingData : ITypingData
+        {
+            private IWriting _pen;
+
+            public WritingToTypingData(IWriting pen)
+            {
+                _pen = pen;
+            }
+            public void Type()
+            {
+                _pen.Write();
+            }
+        }
+    }
+}
