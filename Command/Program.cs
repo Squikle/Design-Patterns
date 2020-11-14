09ba23a3f7e7ca78605ea3aebc988bfe8214f779
Squikle <55331475+Squikle@users.noreply.github.com>
2020-11-14 02:42:22 +0200

    (refact) removed useless directives for Command pattern

--- Command/Program.cs
+++ Command/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Command
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             SwitchController controller = new SwitchController(new Tv());
             controller.Action(new OnCommand());
